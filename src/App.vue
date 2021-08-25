11262d08a5e0fb72130c5882a8c5a1e1700f5687
cyy <347705996@qq.com>
2021-08-25 19:13:23 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id='app'>
+<myHeader />
+<MyGoods><MyCount /></MyGoods>
+
+<MyFooter />
+  </div>
+</template>
+
+<script>
+import myHeader from './components/myHeader.vue'
+import MyGoods from './components/MyGoods.vue'
+import MyFooter from './components/MyFooter.vue'
+import MyCount from './components/MyCount.vue'
+export default {
+  data () {
+    return {
+
+    }
+  },
+components:{
+  myHeader,
+  MyGoods,
+  MyFooter,
+  MyCount
+  },
+  created () {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+#app{
+  padding-top:50px
+}
+</style>
